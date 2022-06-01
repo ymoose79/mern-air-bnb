12e36628a32520ed900dd6e4792ccc5c5d6284e0
ymoose79 <justin.r.stock@icloud.com>
2022-06-01 13:44:33 -0400

    ...

--- client/lambda_function/airbnb_spoof.js
+++ client/lambda_function/airbnb_spoof.js
@@ -1,41 +1,26 @@
 const MongoClient = require("mongodb").MongoClient;
 
 const MONGODB_URI = process.env.MONGODB_URI;
-const DB_NAME = 'sample_airbnb';
 
-let cachedDb = null;
+let _db;
 
-const connectToDatabase = async (uri) => {
-    // we can cache the access to our database to speed things up a bit
-    // (this is the only thing that is safe to cache here)
-    if (cachedDb) return cachedDb;
-
-    const client = await MongoClient.connect(uri, {
-        useUnifiedTopology: true,
-    });
-
-    cachedDb = client.db(DB_NAME);
-
-    return cachedDb;
-};
-
-const queryDatabase = async (db) => {
-    const listingsAndReviews = await db.collection("listingsAndReviews").find({}).toArray();
-
-    return {
-        statusCode: 200,
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(listingsAndReviews),
-    };
-};
+const client = new MongoClient(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 module.exports.handler = async (event, context) => {
     // otherwise the connection will never complete, since
     // we keep the DB connection alive
     context.callbackWaitsForEmptyEventLoop = false;
-
-    const db = await connectToDatabase(MONGODB_URI);
-    return queryDatabase(db);
-};
+    client.connect(function (err, db) {
+        // Verify we got a good "db" object
+        if (db) {
+            _db = db.db("sample_airbnb");
+            console.log("Successfully connected to MongoDB.");
+        } else {
+            console.log(err)
+        }
+    });
+    return _db;
+}
