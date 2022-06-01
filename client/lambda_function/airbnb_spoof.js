0c97d8ae6d5a769c84ae4e93a102d710cace811f
ymoose79 <justin.r.stock@icloud.com>
2022-06-01 11:51:02 -0400

    ...

--- client/lambda_function/airbnb_spoof.js
+++ client/lambda_function/airbnb_spoof.js
@@ -1,4 +1,4 @@
-const { MongoClient } = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = 'sample_airbnb';
