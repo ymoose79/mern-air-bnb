4f56e75ee025b0e75b2c563f5a13bcc53c67018d
ymoose79 <justin.r.stock@icloud.com>
2022-06-02 12:03:10 -0400

    ... /location

---
+++ client/lambda_function/location.js
@@ -0,0 +1,40 @@
+const { MongoClient } = require("mongodb")
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+
+let client = new MongoClient(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const clientPromise = client.connect();
+
+const queryLocalCity = async (db, local) => {
+
+    const { city } = local
+    const myObj = {
+        "address.market": city
+    }
+
+    const localCity = await db
+        .collection("listingsAndReviews")
+        .find(myObj)
+        .limit(50)
+        .toArray();
+    return {
+        statusCode: 200,
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(localCity)
+    };
+}
+
+module.exports.handler = async (event, context) => {
+    client = await clientPromise;
+    context.callbackWaitsForEmptyEventLoop = false;
+    const local = event.queryStringParameters.location
+    const dbConn = await client.db("sample_airbnb")
+    return queryLocalCity(dbConn, local);
+}
