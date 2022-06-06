210f3e5a09b6e23b5c4a8e6c9d779e783761d18e
ymoose79 <justin.r.stock@icloud.com>
2022-06-06 16:35:05 -0400

    -jost user info to user-data collection

---
+++ server/model/model_user.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const User = new mongoose.Schema({
+    name: { type: String, required: true },
+    userName: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+},
+    { collection: 'user-data' }
+)
+const model = mongoose.model('UserData', User)
+module.exports = model;
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,15 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require('mongoose')
+const User = require("../model/model_user")
 
-// connect to mongodb
-const dbo = require("../db/conn");
+mongoose.connect('mongodb://127.0.0.1:27017/sample_airbnb')
 
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
-
-
-router.route("/register").get(function (req, res) {
+router.route("/api/register").post(async function (req, res) {
+    try {
+        const user = await User.create({
+            name: req.body.name,
+            userName: req.body.userName,
+            email: req.body.email,
+            password: req.body.password
+        })
+        res.send("great success ")
+    } catch (error) {
+        console.log("error: ", error)
+    }
     res.send('Register ')
+});
+
+router.route("/api/login").post(async function (req, res) {
+    const user = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    })
+    if (user) {
+        return res.send(user)
+    } else {
+        return console.log("error user block")
+    }
 });
 
 module.exports = router;
