41037dea43c53ed1c3553b9d18e89e98ad858035
ymoose79 <justin.r.stock@icloud.com>
2022-06-20 15:10:52 -0400

    - install google auth - fix app ReactDom error - style modal form

--- client/src/App.js
+++ client/src/App.js
@@ -6,20 +6,27 @@ import NoMatch from "./Routes/NoMatch.js";
 import Location from "./Routes/Location.js";
 import LocationDetails from "./Routes/LocationDetails.js";
 import Rentee from "./Routes/Rentee.js";
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 
 
 function App() {
+
+    const { REACT_APP_CLIENT_ID } = process.env
+    console.log(process.env)
+
     return (
         <div className='container-lg'>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path=":city" element={<Location />} />
-                <Route path=":city/:id" element={<LocationDetails />} />
-                <Route path="/rentee" element={<Rentee />} />
-                <Route path="*" element={<NoMatch />} />
-            </Routes>
+            <GoogleOAuthProvider clientId={REACT_APP_CLIENT_ID}>
+                <NavBar />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path=":city" element={<Location />} />
+                    <Route path=":city/:id" element={<LocationDetails />} />
+                    <Route path="/rentee" element={<Rentee />} />
+                    <Route path="*" element={<NoMatch />} />
+                </Routes>
+            </GoogleOAuthProvider>
         </div>
     )
 }
--- client/src/components/ReactModal.js
+++ client/src/components/ReactModal.js
@@ -7,18 +7,54 @@
     continue w/ FB, Google, Apple, email...
 */
 
-import React from 'react'
+import React, { useState } from 'react'
 import Modal from "react-bootstrap/Modal";
+import { GoogleLogin } from '@react-oauth/google';
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const ReactModal = ({ open, openModalState }) => {
 
-    function check() { console.log('apple') }
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [signUp, setSignUp] = useState(true)
+
+    function check() { console.log(name) }
+    const handleChange = (e) => { e.preventDefault(); console.log(e.target); setName(e.target.value) }
+    const handleSubmit = () => { }
+    const switchMode = () => { setSignUp(!signUp) }
 
     return (
+
+
         <Modal show={open} centered aria-labelledby="contained-modal-title-vcenter">
-            <Modal.Header id="contained-modal-title-vcenter">Hi </Modal.Header>
-            <Modal.Body><button onClick={check}>facebook</button></Modal.Body>
+            <Modal.Header id="contained-modal-title-vcenter">Welcome
+                <button className='btn' onClick={switchMode}>{signUp ? "Click here to Login" : "Click here to register"}</button></Modal.Header>
+            <Modal.Body>
+                <form onSubmit={handleSubmit}>
+                    <div className="d-grid gap-2 mx-5">
+                        {signUp &&
+                            <input type="text" className="form-control" value={name} placeholder="Full Name" onChange={handleChange} required={true} />
+                        }
+                        <input type="email" className="form-control" value={email} placeholder="Email" onChange={handleChange} />
+                        <input type="password" className="form-control" value={password} placeholder="Password" onChange={handleChange} />
+                        <button type="submit" className='btn btn-primary'>submit</button>
+                    </div>
+                </form>
+                <hr />
+                <h6 className="text-center">or</h6>
+                <div className='d-grid gap-2'>
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />
+                    <button onClick={check}>facebook</button>
+                </div>
+            </Modal.Body>
             <Modal.Footer>This is the footer
                 <button onClick={openModalState(open)}>close</button>
             </Modal.Footer>
