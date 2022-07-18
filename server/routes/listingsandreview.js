35b38242cc6d85d91f800776e112b3009ae0ff55
ymoose79 <justin.r.stock@icloud.com>
2022-07-18 07:55:14 -0400

    - remove unnecessary code from listingsandreview.js

--- server/routes/listingsandreview.js
+++ server/routes/listingsandreview.js
@@ -24,58 +24,6 @@ recordRoutes.route("/").get(function (req, res) {
       res.json(result);
     });
 });
-// // -----> TODO TODO TODO TODO -> adjust BSON type err on OBJECT ID
-// // This section will help you get a single record by id
-// recordRoutes.route("/:id").get(function (req, res) {
-//   let db_connect = dbo.getDb();
-//   let myquery = { _id: ObjectId(req.params.id) };
-//   db_connect
-//     .collection("listingsAndReviews")
-//     .findOne(myquery, function (err, result) {
-//       if (err) throw err;
-//       res.json(result);
-//     });
-// });
-
-// This section will help you create a new record.
-recordRoutes.route("/add").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  // values need to be adjusted <------------------------
-  let myobj = {
-    name: req.body.name,
-    summary: req.body.summary,
-    description: req.body.description,
-  };
-  db_connect.collection("listingsAndReviews").insertOne(myobj, function (err, res) {
-    if (err) throw err;
-    response.json(res);
-  });
-});
-
-// This section will help you update a record by id.
-recordRoutes.route("/update/:id").post(function (req, response) {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  let newvalues = {
-    // values need to be adjusted <------------------------
-    $set: {
-      name: req.body.name,
-      summary: req.body.summary,
-      description: req.body.description,
-    },
-  }
-});
-
-// This section will help you delete a record
-recordRoutes.route("/:id").delete((req, response) => {
-  let db_connect = dbo.getDb();
-  let myquery = { _id: ObjectId(req.params.id) };
-  db_connect.collection("listingsAndReviews").deleteOne(myquery, function (err, obj) {
-    if (err) throw err;
-    console.log("1 document deleted");
-    response.json(obj);
-  });
-});
 
 recordRoutes.route("/location").get(function (req, res) {
 
